e6bd4225eee76c865de545a69ad006adad555bd1
MadSeas0n <p1no4et@yandex.ru>
2015-10-14 10:06:44 +0300

    commit

--- views.py
+++ views.py
@@ -1,98 +1,156 @@
-from forms import AddTaskForm
+###############
+####IMPORTS####
+###############
+from forms import AddTaskForm, RegisterForm, LoginForm
 from functools import wraps
 
-from flask import Flask, flash, redirect, render_template, request,session,url_for, g
+from flask import Flask, flash, redirect, render_template, request, session, url_for, g
 from flask.ext.sqlalchemy import SQLAlchemy
 
-#conf
+###############
+####CONFIG#####
+###############
 app = Flask(__name__)
 app.config.from_object('_config')
 db = SQLAlchemy(app)
 
-from models import Task
+from models import Task, User
 
-#helpers
+###############
+####HELPERS####
+###############
+
 
 def login_required(test):
-	@wraps(test)
-	def wrap(*args, **kwargs):
-		if 'logged_in' in session:
-			return test(*args, **kwargs)
-		else:
-			flash('You need to login first')
-			return redirect(url_for('login'))
-	return wrap
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first')
+            return redirect(url_for('login'))
+    return wrap
 
-#routes
-#logout
+###############
+#####LOGOUT####
+###############
+
+
 @app.route('/logout/')
 def logout():
-	session.pop('logged_in', None)
-	flash('Goodbye!')
-	return redirect(url_for('login'))
+    session.pop('logged_in', None)
+    flash('Goodbye!')
+    return redirect(url_for('login'))
 
-#login
+###############
+#####LOGIN#####
+###############
+
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
-			error = 'Invalid credentials. Please try again'
-			return render_template('login.html', error=error)
-		else:
-			session['logged_in'] = True
-			flash('Welcome')
-			return redirect(url_for('tasks'))
-	return render_template('login.html')
+    error = None
+    form = LoginForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.filter_by(name=request.form['name']).first()
+            if user is not None and user.password == request.form['password']:
+                session['logged_in'] = True
+                session['user_id'] = user.id
+                flash('Welcome!')
+                return redirect(url_for('tasks'))
+            else:
+                error = 'Invalid username or password.'
+        else:
+            error = 'Both fields are required.'
+    return render_template('login.html', form=form, error=error)
 
-#tasks
+###############
+###REGISTER####
+###############
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    error = None
+    form = RegisterForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_user = User(
+                form.name.data,
+                form.email.data,
+                form.password.data,
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Thanks for registering. Please login.')
+            return redirect(url_for('login'))
+    return render_template('register.html', form=form, error=error)
+
+###############
+#####TASKS#####
+###############
+
+
 @app.route('/tasks/')
 @login_required
 def tasks():
-	open_tasks = db.session.query(Task).filter_by(status='1').order_by(Task.due_date.asc())
-	closed_tasks = db.session.query(Task).filter_by(status='0').order_by(Task.due_date.asc())
-	return render_template(
-		'tasks.html',
-		form=AddTaskForm(request.form),
-		open_tasks=open_tasks,
-		closed_tasks=closed_tasks
-		)
+    open_tasks = db.session.query(Task).filter_by(
+        status='1').order_by(Task.due_date.asc())
+    closed_tasks = db.session.query(Task).filter_by(
+        status='0').order_by(Task.due_date.asc())
+    return render_template(
+        'tasks.html',
+        form=AddTaskForm(request.form),
+        open_tasks=open_tasks,
+        closed_tasks=closed_tasks
+    )
 
 
-#add_task
-@app.route('/add/', methods = ['POST'])
+###############
+###ADD_TASK####
+###############
+@app.route('/add/', methods=['POST'])
 @login_required
 def new_task():
-	form = AddTaskForm(request.form)
-	if request.method == 'POST':
-		if form.validate_on_submit():
-			new_task = Task(
-				form.name.data,
-				form.due_date.data,
-				form.priority.data,
-				'1'
-				)
-			db.session.add(new_task)
-			db.session.commit()
-			flash('New entry was successfully posted!')
-	return redirect(url_for('tasks'))
+    form = AddTaskForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_task = Task(
+                form.name.data,
+                form.due_date.data,
+                form.priority.data,
+                '1'
+            )
+            db.session.add(new_task)
+            db.session.commit()
+            flash('New entry was successfully posted!')
+    return redirect(url_for('tasks'))
 
-#mark as complete
+#####################
+####MARK_COMPLETE####
+#####################
+
+
 @app.route('/complete/<int:task_id>/')
 @login_required
 def complete(task_id):
-	new_id = task_id
-	db.session.query(Task).filter_by(task_id=new_id).update({"status":"0"})
-	db.session.commit()
-	flash('The task was marked as complete')
-	return redirect(url_for('tasks'))
+    new_id = task_id
+    db.session.query(Task).filter_by(task_id=new_id).update({"status": "0"})
+    db.session.commit()
+    flash('The task was marked as complete')
+    return redirect(url_for('tasks'))
 
-#delete task
+#####################
+####DELETE_TASK####
+#####################
+
+
 @app.route('/delete/<int:task_id>')
 @login_required
 def delete(task_id):
-	new_id = task_id
-	db.session.query(Task).filter_by(task_id=new_id).delete()
-	db.session.commit()
-	flash('The task was successfully deleted')
-	return redirect(url_for('tasks'))
+    new_id = task_id
+    db.session.query(Task).filter_by(task_id=new_id).delete()
+    db.session.commit()
+    flash('The task was successfully deleted')
+    return redirect(url_for('tasks'))
